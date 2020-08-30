49cf68383f0fab609f71ce7af859c0b732b2531d
Shivanand Biradar <extern.shivanand.biradar@astech-auto.de>
2020-08-30 20:48:13 +0200

    update the subscribe for get dept in controller

--- spring-reactive-rest-client/src/main/java/com/shiv/api/StudentController.java
+++ spring-reactive-rest-client/src/main/java/com/shiv/api/StudentController.java
@@ -5,19 +5,26 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.reactivex.Flowable;
+
 import com.shiv.service.DataService;
 
 @RestController
 public class StudentController {
 	
-//    private static final Logger LOG = LoggerFactory.getLogger(StudentController.class);
+    private static final Logger LOG = LoggerFactory.getLogger(StudentController.class);
 
 	@Autowired
 	private DataService dataService;
 	
+	private String result;
+	
     @GetMapping("/students/dept/{name}")
     public String getDepartments(@PathVariable("name") String name) {
+
+    	dataService.getStudentsDepartment(name).subscribe((dept,err)->{
+    		if(err != null) { result=" not found!"; } else { result=dept; } });
     	
-        return "Hello " + name + ", You belong to "+dataService.getStudentsDepartment(name);
+       return "Hello " + name + ", Your department is "+result;
     }
 }
